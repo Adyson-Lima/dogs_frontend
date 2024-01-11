e50b82fa204b7c0143d32e830be8326be93761e9
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-11 15:28:27 -0300

    27° commit - logica saveOrUpdate implementada

--- src/pages/NewUpdate/index.js
+++ src/pages/NewUpdate/index.js
@@ -1,8 +1,59 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
 import React, {useState, useEffect} from "react";
-// import api from '../../services/api';
+import api from '../../services/api';
 
 export default function NewUpdate(){
+
+  const[breed, setBreed] = useState('');
+  const[size, setSize] = useState('');
+  const navigate = useNavigate();
+
+  // identificador dog_id definido na rota
+  const{dog_id} = useParams();
+
+  async function saveOrUpdate(e){
+
+    e.preventDefault();
+
+    const data = {breed, size};
+
+    if (dog_id === '0') {
+      try {
+        await api.post('api/v1/dogs', data, {});
+        navigate('/');        
+      } catch (error) {
+        alert('Erro ao salvar');        
+      }      
+    } else {
+      try {
+        await api.patch(`api/v1/dogs/${dog_id}`, data, {});
+        navigate('/');        
+      } catch (error) {
+        alert('Erro ao atualizar');        
+      }      
+    }
+  }
+
+  // Carrega registro específico na api e seta dados para atualização
+  async function loadDog(){
+    try {
+      const response = await api.get(`api/v1/dogs/${dog_id}`,{});
+      setBreed(response.data.breed);
+      setSize(response.data.size);      
+    } catch (error) {
+      alert("Erro ao buscar registro na api");
+      navigate('/');      
+    }
+  }
+
+  // Chama loadDog para preencher form
+  useEffect(() => {
+    if (dog_id === '0') {
+      return;      
+    } else {
+      loadDog();      
+    }
+  }, [dog_id]);
 
   return(
 
@@ -15,20 +66,22 @@ export default function NewUpdate(){
         <Link data-testid="mylink" className="btn btn-dark" 
         style={{marginBottom: '5px'}} to="/">Home</Link>
 
-        <form data-testid="myform">
+        <form data-testid="myform" onSubmit={saveOrUpdate}>
 
           <div className="form-group">
-            <label htmlFor="###">###</label>
-            <input data-testid="input1" id="###" type="text" 
+            <label htmlFor="breed">Raça</label>
+            <input data-testid="input1" id="breed" type="text" 
             style={{marginBottom: '20px'}} className="form-control" 
-            placeholder="###"></input>
+            placeholder="Raça" value={breed}
+            onChange={e => setBreed(e.target.value)}></input>
           </div>
 
           <div className="form-group">
-            <label htmlFor="###">###</label>
-            <input data-testid="input2" id="###" type="text" 
+            <label htmlFor="size">Tamanho</label>
+            <input data-testid="input2" id="size" type="text" 
             style={{marginBottom: '20px'}} className="form-control" 
-            placeholder="###"></input>
+            placeholder="Tamanho" value={size}
+            onChange={e => setSize(e.target.value)}></input>
           </div>
 
           <button data-testid="btnenviar" type="submit" className="btn btn-primary">Enviar</button>
