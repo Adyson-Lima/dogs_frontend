c7cfff27c0aed2d59132137af77e71cdf65af603
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-11 14:08:21 -0300

    6° commit - tdd tela Dogs: Existe tabela em Dogs? RED

--- src/tests/Dogs.test.js
+++ src/tests/Dogs.test.js
@@ -20,4 +20,8 @@ describe('Testes da tela Dogs', () => {
     expect(screen.getByTestId('mylink')).toBeInTheDocument();
   });
 
+  it('Existe tabela em Dogs?', () => {
+    expect(screen.getByTestId('mytable')).toBeInTheDocument();
+  });
+
 });
