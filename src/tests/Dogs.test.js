8eb9b1fa21411e619205197e45959cf71545e29c
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-11 14:05:09 -0300

    4° commit - tdd tela Dogs: Existe link Novo em Dogs? RED

--- src/tests/Dogs.test.js
+++ src/tests/Dogs.test.js
@@ -16,4 +16,8 @@ describe('Testes da tela Dogs', () => {
     expect(screen.getByTestId('mycard')).toBeInTheDocument();
   });
 
+  it('Existe link Novo em Dogs?', () => {
+    expect(screen.getByTestId('mylink')).toBeInTheDocument();
+  });
+
 });
