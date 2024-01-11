1d1af78d7d06a691ef858bfe7c8263accb49f352
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-11 14:16:15 -0300

    10° commit - tdd tela Dogs: Existe botão excluir em Dogs? RED

--- src/tests/Dogs.test.js
+++ src/tests/Dogs.test.js
@@ -28,4 +28,8 @@ describe('Testes da tela Dogs', () => {
     expect(screen.getByTestId('mybtn1')).toBeInTheDocument();
   });
 
+  it('Existe botão excluir em Dogs?', () => {
+    expect(screen.getByTestId('mybtn2')).toBeInTheDocument();
+  });
+
 });
